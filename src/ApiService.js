5dc4f549001cb4f33e29e8cb3819246f0f5b261e
maroufCode <ahmed.marouf@milango.app>
2024-06-29 04:31:13 +0300

    set api staging server baseurl

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://knownlege.test/api/';
+const API_BASE_URL = 'https://bnu-api-staging.knownlege.com/api/';
 
 // Set up default config for axios
 const axiosInstance = axios.create({
